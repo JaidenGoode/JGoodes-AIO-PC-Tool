110b8f4baf3bf6510dc7e3cc9b7fd5fff912962d
JaidenGoode <53543614+JaidenGoode@users.noreply.github.com>
2026-03-05 00:20:38 -0800

    chore: sync via JGoode A.I.O PC Tool

--- vite.config.ts
+++ vite.config.ts
@@ -33,6 +33,7 @@ export default defineConfig({
   build: {
     outDir: path.resolve(__dirname, "dist/public"),
     emptyOutDir: true,
+    chunkSizeWarningLimit: 1000,
   },
   server: {
     fs: {
